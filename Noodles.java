4134d9807a4e23a1535a1e4018ce5bcbac255574
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 05:05:39 +0800

    Update Noodles.java

--- Noodles.java
+++ Noodles.java
@@ -1,1 +1,11 @@
+public class Noodles extends Product{
+    public Noodles(
+        String productType,
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super("Noodles", productName, serialNumber, price);
+    }    
+}
 
