7d79bb9eb27607b009a8e7120b450beeb2f4c3b9
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:28:41 +0800

    Update CannedGoods.java

--- CannedGoods.java
+++ CannedGoods.java
@@ -1,1 +1,9 @@
-
+public class CannedGoods extends Product{
+    public CannedGoods(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
