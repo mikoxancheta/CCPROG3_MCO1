c32aa2343abe24f04c4793abfee24177833b3dc1
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:24:41 +0800

    Update Alcohol.java

--- Alcohol.java
+++ Alcohol.java
@@ -1,1 +1,9 @@
-
+public class Alcohol extends Product{
+    public Alcohol(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
