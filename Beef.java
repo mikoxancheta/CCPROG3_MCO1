0002bd65dacd8d1b8abf4d1c3c1cc9385fb36f15
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:24:56 +0800

    Update Beef.java

--- Beef.java
+++ Beef.java
@@ -1,1 +1,9 @@
-
+public class Beef extends Product{
+    public Beef(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
