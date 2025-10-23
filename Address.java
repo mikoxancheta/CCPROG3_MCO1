7231093ace4e1a01dc442b972eabd5562fbfa167
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 05:12:40 +0800

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,1 +1,27 @@
+public class Address {
+    private String floor;
+    private String grouping;
+    private int number;
 
+    public Address(
+        String floor,
+        String grouping, 
+        int number
+    ) {
+        this.floor = floor;
+        this.grouping = grouping;
+        this.number = number;
+    }
+
+    public String getFloor() {
+        return this.floor;
+    }
+
+    public String getGrouping() {
+        return this.grouping;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
