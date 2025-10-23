7be98ac5369432b827e9f125f5f4db2ad27a423e
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:27:45 +0800

    Update Seafood.java

--- Seafood.java
+++ Seafood.java
@@ -1,1 +1,9 @@
-
+public class Seafood extends Product{
+    public Seafood(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
