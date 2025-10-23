1d11cb4fc5509a5d17477911277382ba4179c056
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:28:19 +0800

    Update Softdrink.java

--- Softdrink.java
+++ Softdrink.java
@@ -1,1 +1,9 @@
-
+public class SoftDrink extends Product{
+    public SoftDrink(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
