a2eb17f9214516c880272e400c66ecae875f354c
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:26:37 +0800

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,1 +1,9 @@
-
+public class Fruit extends Product{
+    public Fruit(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }
+}
