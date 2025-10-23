1b067b0c988b53b0a9e3b42904f674c326c6fc7f
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 05:13:49 +0800

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,1 +1,26 @@
+public class Position{
+    private int x;
+    private int y;
 
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getx() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int X) {
+        this.x = x;
+
+    }
+
+    public void setY(int Y) {
+        this.y = y;
+    }
+}
