828280e592d0de4473b7c3a3119be84466e096c8
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:53:42 +0800

    Update Cereal.java

--- Cereal.java
+++ Cereal.java
@@ -1,9 +1,10 @@
 public class Cereal extends Product{
     public Cereal(
+        String productType,
         String productName,
         String serialNumber,
         double price
     ) {
-        super(productName, serialNumber, price, null);
+        super("Cereal", productName, serialNumber, price);
     }
 }
