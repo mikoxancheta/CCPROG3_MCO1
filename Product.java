30981033157f07f690abae4e251dca15bf80bf22
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:23:29 +0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,1 +1,35 @@
+//Super class
+public class Product{
+    String productName;
+    String serialNumber;
+    double price;
+    String productType;
 
+    public Product(
+        String productName,
+        String serialNumber,
+        double price,
+        String productType
+    ) {
+        this.productName = productName;
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.productType = productType;
+    }
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public String getSerialNumber() {
+        return this.serialNumber;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getProductType() {
+        return this.productType;
+    }
+}
