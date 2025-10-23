a4dca973bd44e10500f005bf86c32dceecabe46b
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 05:03:17 +0800

    Update Chicken.java

--- Chicken.java
+++ Chicken.java
@@ -1,1 +1,10 @@
-
+public class Chicken extends Product{
+    public Chicken(
+        String productType,
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super("Chicken", productName, serialNumber, price);
+    }
+}
