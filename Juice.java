4c3a6c75c7dcacae03c400cd454a275b18bea713
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 04:27:17 +0800

    Update Juice.java

--- Juice.java
+++ Juice.java
@@ -1,1 +1,10 @@
+public class Juice extends Product{
+    public Juice(
+        String productName,
+        String serialNumber,
+        double price
+    ) {
+        super(productName, serialNumber, price, null);
+    }    
+}
 
