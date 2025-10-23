935880cda90745fbe480cc093c01aec57bd97d2c
marianrecio <marian_recio@dlsu.edu.ph>
2025-10-24 05:06:29 +0800

    Update Snacks.java

--- Snacks.java
+++ Snacks.java
@@ -1,9 +1,10 @@
 public class Snacks extends Product{
     public Snacks(
+        String productType,
         String productName,
         String serialNumber,
         double price
     ) {
-        super(productName, serialNumber, price, null);
+        super("Snacks", productName, serialNumber, price);
     }
 }
