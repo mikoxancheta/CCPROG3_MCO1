f887f6f71b427c1d1637b82686ed3c105de5833f
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 14:38:03 +0800

    Update Service.java
    Added Service class to represent various service areas in the supermarket.

--- Service.java
+++ Service.java
@@ -1,1 +1,18 @@
+// Represents a service area in the supermarket (i.e. Entrance, Exit, BasketStation, CartStation, ProductSearch, and CheckoutCounter).
 
+public class Service extends Amenity {
+    private String type;
+
+    public Service(Position position, String type) {
+        super(position); 
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void performService(Shopper shopper) {
+        // Implementation will vary depending on subclass (Entrance, Exit, etc.)
+    }
+}
