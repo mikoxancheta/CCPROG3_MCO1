c355f3bfc1b565cb88adb42f53b73fa10d3a028e
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 14:08:28 +0800

    Update Amenity.java

--- Amenity.java
+++ Amenity.java
@@ -1,1 +1,21 @@
+public abstract class Amenity {
+    private Position position;
 
+    public Amenity(Position position) {
+        this.position = position;
+    }
+
+    // Getter for position
+    public Position getPosition() {
+        return position;
+    }
+
+    // Setter for position (optional, in case position can change)
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    // Abstract method for shopper interaction
+    // Each subclass will define what happens when a shopper interacts with it.
+    public abstract void interact(Shopper shopper);
+}
