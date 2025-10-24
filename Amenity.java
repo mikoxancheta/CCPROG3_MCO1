0366cdacf7b4e3ccdd44b1f4e79f87fa0a96c4d7
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 14:41:59 +0800

    Update Amenity.java

--- Amenity.java
+++ Amenity.java
@@ -1,12 +1,29 @@
-// Represents any interactable object inside the supermarket.
-// Used as a base class for Display and Service.
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Amenity class represents any interactable object inside the supermarket.
+ * This includes both displays and services that are placed at specific positions
+ * on the supermarket floor. It serves as the base class for Display and Service.
+ */
 public class Amenity {
+
+    /** The position of this amenity on the supermarket floor. */
     private Position position;
-    
+
+    /**
+     * Constructs an Amenity with a specified position.
+     *
+     * @param position the position of the amenity on the floor
+     */
     public Amenity(Position position) {
         this.position = position;
     }
 
+    /**
+     * Returns the position of this amenity.
+     *
+     * @return the position of the amenity
+     */
     public Position getPosition() {
         return position;
     }
