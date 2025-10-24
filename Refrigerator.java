2788def9211fadcada8e828aca8d81d13198f22f
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 15:03:33 +0800

    Update Refrigerator.java

--- Refrigerator.java
+++ Refrigerator.java
@@ -1,1 +1,19 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Refrigerator class represents a display used for milk,
+ * frozen food, and cheese. It inherits from Display.
+ */
+public class Refrigerator extends Display {
 
+    /**
+     * Constructs a Refrigerator with a specified position, address, and capacity.
+     *
+     * @param position the position of the refrigerator on the floor
+     * @param address the address or section label of the refrigerator
+     * @param capacity the maximum number of products that can be displayed
+     */
+    public Refrigerator(Position position, Address address, int capacity) {
+        super(position, address, capacity);
+    }
+}
