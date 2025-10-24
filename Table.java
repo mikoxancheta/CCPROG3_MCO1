52f488125628d8dd4ca6cd87235b357d648f6acb
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 15:00:14 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,1 +1,20 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Table class represents a display area used for fruits and other
+ * products that do not require refrigeration. It inherits all attributes
+ * and behavior from the Display class.
+ */
+public class Table extends Display {
 
+    /**
+     * Constructs a Table with a specified position, address, and capacity.
+     *
+     * @param position the position of the table on the floor
+     * @param address the address or section label of the table
+     * @param capacity the maximum number of products that can be displayed
+     */
+    public Table(Position position, Address address, int capacity) {
+        super(position, address, capacity);
+    }
+}
