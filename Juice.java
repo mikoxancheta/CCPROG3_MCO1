13a413a7b46dd97f0e275306bcb01f245dd2a6f7
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:39:52 +0800

    Update Juice.java

--- Juice.java
+++ Juice.java
@@ -1,4 +1,20 @@
-public class Juice extends Product{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Juice product in the supermarket.
+ * It inherits from the Product class and initializes the product type as "Juice".
+ */
+public class Juice extends Product {
+
+    /**
+     * Constructs a new Juice product with the specified details.
+     * The product type is automatically set to "Juice".
+     * 
+     * @param productType   The type of the product (ignored, always set to "Juice")
+     * @param productName   The name of the product
+     * @param serialNumber  The unique serial number of the product
+     * @param price         The price of the product
+     */
     public Juice(
         String productType,
         String productName,
