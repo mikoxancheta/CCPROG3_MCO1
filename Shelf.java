5577bfb52cb3c4a8b722d0889d347f961c9eae45
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 14:58:55 +0800

    Update Shelf.java

--- Shelf.java
+++ Shelf.java
@@ -1,1 +1,21 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Shelf class represents a display area in the supermarket
+ * that stores products such as snacks, canned goods, noodles, cereal,
+ * softdrink, juice, alcohol, and condiments.
+ * A shelf has a specific position, address, and capacity, inherited from Display.
+ */
+public class Shelf extends Display {
 
+    /**
+     * Constructs a Shelf with a specified position, address, and capacity.
+     *
+     * @param position the position of the shelf on the floor
+     * @param address the address or section label of the shelf
+     * @param capacity the maximum number of products that can be displayed
+     */
+    public Shelf(Position position, Address address, int capacity) {
+        super(position, address, capacity);
+    }
+}
