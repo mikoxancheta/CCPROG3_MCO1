8dd1445631f1a628d27e0d96c20b3d71a0b40292
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:28:48 +0800

    Update Basket.java

--- Basket.java
+++ Basket.java
@@ -1,5 +1,16 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Basket equipment in the supermarket.
+ * It inherits from the Equipment class and initializes the equipment 
+ * type as "basket" with a default capacity of 15.
+ */
 public class Basket extends Equipment {
-    public Basket(){
-        super("basket",15);
+
+    /**
+     * Constructs a new Basket with default type "basket" and capacity 15.
+     */
+    public Basket() {
+        super("basket", 15);
     }
 }
