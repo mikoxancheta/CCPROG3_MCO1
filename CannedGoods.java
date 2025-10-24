324132020ee6d6f7fdb4bf1d3155a96aaab13897
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:30:03 +0800

    Update CannedGoods.java

--- CannedGoods.java
+++ CannedGoods.java
@@ -1,4 +1,20 @@
-public class CannedGoods extends Product{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Canned Goods product in the supermarket.
+ * It inherits from the Product class and initializes the product type as "Canned Goods".
+ */
+public class CannedGoods extends Product {
+
+    /**
+     * Constructs a new Canned Goods product with the specified details.
+     * The product type is automatically set to "Canned Goods".
+     * 
+     * @param productType   The type of the product (ignored, always set to "Canned Goods")
+     * @param productName   The name of the product
+     * @param serialNumber  The unique serial number of the product
+     * @param price         The price of the product
+     */
     public CannedGoods(
         String productType,
         String productName,
