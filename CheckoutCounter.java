8fefb5f0a18900d131f1412b8d748ed89a2ed0e3
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 15:16:18 +0800

    Update CheckoutCounter.java

--- CheckoutCounter.java
+++ CheckoutCounter.java
@@ -1,1 +1,28 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The CheckoutCounter class represents the area where shoppers
+ * pay for their selected items. After checkout, the shopper's
+ * equipment (basket or cart) is returned to its respective station.
+ * This class extends Service.
+ */
+public class CheckoutCounter extends Service {
 
+    /**
+     * Constructs a CheckoutCounter with a specified position.
+     *
+     * @param position the position of the checkout counter on the floor
+     */
+    public CheckoutCounter(Position position) {
+        super(position, "Checkout Counter");
+    }
+
+    /**
+     * Performs the checkout service for a shopper.
+     *
+     * @param shopper the shopper checking out their items
+     */
+    public void performService(Shopper shopper) {
+        // Implementation for MCO1 can be added later
+    }
+}
