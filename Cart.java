95fdd64d52bd666fd20225dc16a4c96c758f5c95
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:30:31 +0800

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,6 +1,16 @@
-public class Cart extends Equipment{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Cart equipment in the supermarket.
+ * It inherits from the Equipment class and initializes the equipment 
+ * type as "cart" with a default capacity of 30.
+ */
+public class Cart extends Equipment {
+
+    /**
+     * Constructs a new Cart with default type "cart" and capacity 30.
+     */
     public Cart() {
-        super("cart",30);
+        super("cart", 30);
     }
 }
-
