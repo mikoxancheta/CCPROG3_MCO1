cc16dc397cd6236bed0697d0a15240273c4a903e
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:29:33 +0800

    Update Beef.java

--- Beef.java
+++ Beef.java
@@ -1,10 +1,26 @@
-public class Beef extends Product{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Beef product in the supermarket.
+ * It inherits from the Product class and initializes the product type as "Beef".
+ */
+public class Beef extends Product {
+
+    /**
+     * Constructs a new Beef product with the specified details.
+     * The product type is automatically set to "Beef".
+     * 
+     * @param productType   The type of the product (ignored, always set to "Beef")
+     * @param productName   The name of the product
+     * @param serialNumber  The unique serial number of the product
+     * @param price         The price of the product
+     */
     public Beef(
         String productType,
         String productName,
         String serialNumber,
         double price
     ) {
-        super("Beef",productName, serialNumber, price);
+        super("Beef", productName, serialNumber, price);
     }
 }
