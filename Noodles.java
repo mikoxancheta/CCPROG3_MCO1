49fd7531d718367f81bdc601b9e3c6b4c16c73ee
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:40:22 +0800

    Update Noodles.java

--- Noodles.java
+++ Noodles.java
@@ -1,4 +1,20 @@
-public class Noodles extends Product{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Noodles product in the supermarket.
+ * It inherits from the Product class and initializes the product type as "Noodles".
+ */
+public class Noodles extends Product {
+
+    /**
+     * Constructs a new Noodles product with the specified details.
+     * The product type is automatically set to "Noodles".
+     * 
+     * @param productType   The type of the product (ignored, always set to "Noodles")
+     * @param productName   The name of the product
+     * @param serialNumber  The unique serial number of the product
+     * @param price         The price of the product
+     */
     public Noodles(
         String productType,
         String productName,
@@ -8,4 +24,3 @@ public class Noodles extends Product{
         super("Noodles", productName, serialNumber, price);
     }    
 }
-
