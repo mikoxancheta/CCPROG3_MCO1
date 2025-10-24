90ba34df7a146aadd946602d93c8e10ce7218080
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 14:45:07 +0800

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,11 +1,33 @@
-// Represents any product display inside the supermarket (i.e., Table, ChilledCounter, Refrigerator, and Shelf). 
 import java.util.ArrayList;
 
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Display class represents a display area within the supermarket that holds
+ * products. Examples of displays include tables, chilled counters, refrigerators, and
+ * shelves. Each display has a specific position, address, and capacity,
+ * and contains multiple products that shoppers can view or take.
+ *
+ * This class extends Amenity and is used as a base for specific display types.
+ */
 public class Display extends Amenity {
-    private Address address;
-    private int capacity;                
-    private ArrayList<Product> products; 
 
+    /** The address or section identifier of this display. */
+    private Address address;
+
+    /** The maximum number of products that can be displayed. */
+    private int capacity;
+
+    /** The list of products currently placed on the display. */
+    private ArrayList<Product> products;
+
+    /**
+     * Constructs a Display with a specified position, address, and capacity.
+     *
+     * @param position the position of the display on the floor
+     * @param address the address or section label of the display
+     * @param capacity the maximum number of products that can be held
+     */
     public Display(Position position, Address address, int capacity) {
         super(position);
         this.address = address;
@@ -13,20 +35,40 @@ public class Display extends Amenity {
         this.products = new ArrayList<>();
     }
 
+    /**
+     * Adds a product to the display if there is available capacity.
+     *
+     * @param product the product to add to the display
+     */
     public void addProduct(Product product) {
         if (products.size() < capacity) {
             products.add(product);
         }
     }
-  
+
+    /**
+     * Removes a product from the display.
+     *
+     * @param product the product to remove from the display
+     */
     public void removeProduct(Product product) {
         products.remove(product);
     }
 
+    /**
+     * Returns the number of available product slots on the display.
+     *
+     * @return the number of remaining available slots
+     */
     public int getAvailableSlots() {
         return capacity - products.size();
     }
 
+    /**
+     * Returns the list of products currently on the display.
+     *
+     * @return a list of products displayed
+     */
     public ArrayList<Product> getProducts() {
         return products;
     }
