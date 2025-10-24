9bfeb36cff8e4bec155c4f438f5ea3507c646176
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 17:39:22 +0800

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,4 +1,20 @@
-public class Fruit extends Product{
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ * 
+ * This class represents a Fruit product in the supermarket.
+ * It inherits from the Product class and initializes the product type as "Fruit".
+ */
+public class Fruit extends Product {
+
+    /**
+     * Constructs a new Fruit product with the specified details.
+     * The product type is automatically set to "Fruit".
+     * 
+     * @param productType   The type of the product (ignored, always set to "Fruit")
+     * @param productName   The name of the product
+     * @param serialNumber  The unique serial number of the product
+     * @param price         The price of the product
+     */
     public Fruit(
         String productType,
         String productName,
