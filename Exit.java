adf8d247496f133de8c5ae9ed722353f5cb1cd1b
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 15:13:05 +0800

    Update Exit.java
    Add Exit class to represent supermarket exit point

--- Exit.java
+++ Exit.java
@@ -1,1 +1,28 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The Exit class represents the exit point of the supermarket.
+ * When a shopper exits, their visit is finalized and any remaining
+ * equipment is returned to the supermarket.
+ * This class extends Service.
+ */
+public class Exit extends Service {
 
+    /**
+     * Constructs an Exit with a specified position.
+     *
+     * @param position the position of the exit on the floor
+     */
+    public Exit(Position position) {
+        super(position, "Exit");
+    }
+
+    /**
+     * Performs the exit service for a shopper, finalizing their visit.
+     *
+     * @param shopper the shopper leaving the supermarket
+     */
+    public void performService(Shopper shopper) {
+        // Implementation for MCO1 can be added later
+    }
+}
