318f3ca2b2b699e7a6d1b05e569a3d1be29635ec
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 19:27:38 +0800

    Update Refrigerator.java

--- Refrigerator.java
+++ Refrigerator.java
@@ -14,6 +14,11 @@ public class Refrigerator extends Display {
      * @param capacity the maximum number of products that can be displayed
      */
     public Refrigerator(Position position, Address address, int capacity) {
-        super(position, address, capacity);
+        super(position, address, capacity, "Refrigerator");
+    }
+
+    @Override
+    public void interact(Shopper shopper) {
+        // Interaction logic
     }
 }
