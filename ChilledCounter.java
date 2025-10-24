5639adb73d8b97d8e94cd4ea8b7229e943d48e56
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-10-24 15:01:16 +0800

    Update ChilledCounter.java

--- ChilledCounter.java
+++ ChilledCounter.java
@@ -1,1 +1,19 @@
+/**
+ * CCPROG3 MCO1 - Supermarket Simulator
+ *
+ * The ChilledCounter class represents a refrigerated display counter
+ * used for products such as seafood, chicken, and beef. It inherits from Display.
+ */
+public class ChilledCounter extends Display {
 
+    /**
+     * Constructs a ChilledCounter with a specified position, address, and capacity.
+     *
+     * @param position the position of the chilled counter on the floor
+     * @param address the address or section label of the chilled counter
+     * @param capacity the maximum number of products that can be displayed
+     */
+    public ChilledCounter(Position position, Address address, int capacity) {
+        super(position, address, capacity);
+    }
+}
